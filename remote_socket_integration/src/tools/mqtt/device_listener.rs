4cadfd9c496f4d75d03358052f7774a38f68fc9b
Jonas Greifenhain <jonas@greifenhain.eu>
2020-12-13 07:31:24 +0100

    create path for mqtt

--- remote_socket_integration/src/tools/mqtt/device_listener.rs
+++ remote_socket_integration/src/tools/mqtt/device_listener.rs
@@ -4,3 +4,10 @@ use crate::config::Configuration;
 pub fn thread_listener_from_config(conf: &Configuration) {
 	
 }
+
+fn create_mqtt_set_path(dev: &remote_socket::RemoteSocket) -> String{
+	let mname = dev.get_name().replace(" ", "_");
+	
+	return format!("homeassistant/switch/{}/set", mname);
+}
+
