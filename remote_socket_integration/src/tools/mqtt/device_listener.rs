438ca9eba8e431208e6ada77c6b3bd33f2d0f14e
Jonas Greifenhain <cadivus@daverkomp.de>
2020-12-13 14:58:03 +0100

    listen to mqtt for instructions

--- remote_socket_integration/src/tools/mqtt/device_listener.rs
+++ remote_socket_integration/src/tools/mqtt/device_listener.rs
@@ -1,8 +1,39 @@
 use crate::config::Configuration;
+use crate::devices::remote_socket;
+use crate::tools::mqtt::device_controller;
+use crate::tools::serialport::state_sender;
+
+use rumqtt::{MqttClient, QoS};
+use std::thread;
 
 
 pub fn thread_listener_from_config(conf: &Configuration) {
+	let devices = conf.get_devices();
 	
+	for dev in devices.into_iter() {
+		let path = create_mqtt_set_path(&dev);
+		let mqtt_options = conf.get_mqtt_options_with_id(&path.replace("/", "_"));
+		let port = conf.get_get_serialport_arc();
+		
+		thread::spawn(move || {
+			let (mut mqtt_client, notifications) = MqttClient::start(mqtt_options).unwrap();
+			mqtt_client.subscribe(path.clone(), QoS::AtLeastOnce).unwrap();
+			
+			
+			
+			for notification in notifications {
+				match notification {
+					rumqtt::Notification::Publish(nnn) => {
+						let dev_state = String::from_utf8_lossy(&nnn.payload);
+						
+						device_controller::apply_new_state(&mut mqtt_client, &dev, dev_state.to_string());
+						state_sender::write_to_serial(port.clone(), &dev, dev_state.to_string());
+					},
+					_ => println!("{} will nicht", path.clone()),
+				}
+			}
+		});
+	}
 }
 
 fn create_mqtt_set_path(dev: &remote_socket::RemoteSocket) -> String{
