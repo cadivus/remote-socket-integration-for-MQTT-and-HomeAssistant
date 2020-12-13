ed9a0c9e86c63f4d3983a333c9d4c9a9aa95cd4a
Jonas Greifenhain <jonas@greifenhain.eu>
2020-12-13 06:39:59 +0100

    add function for creating threads that listen for commands

---
+++ remote_socket_integration/src/tools/mqtt/device_listener.rs
@@ -0,0 +1,6 @@
+use crate::config::Configuration;
+
+
+pub fn thread_listener_from_config(conf: &Configuration) {
+	
+}
--- remote_socket_integration/src/tools/mqtt/mod.rs
+++ remote_socket_integration/src/tools/mqtt/mod.rs
@@ -1,2 +1,3 @@
 pub mod announce_devices;
+pub mod device_listener;
 pub mod device_controller;
