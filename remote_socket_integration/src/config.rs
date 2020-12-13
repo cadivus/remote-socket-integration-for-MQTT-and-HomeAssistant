210ec53289084902a12f8450856842570b6a64e6
Jonas Greifenhain <jonas@greifenhain.eu>
2020-12-13 06:06:10 +0100

    add mqtt options

--- remote_socket_integration/src/config.rs
+++ remote_socket_integration/src/config.rs
@@ -14,3 +14,49 @@ pub struct Configuration {
 	brokeruser: String,
 	brokerpassword: String,
 }
+
+impl Configuration {
+	pub fn get_devices(&self) -> Vec<remote_socket::RemoteSocket> {
+		self.devices.clone()
+	}
+	
+	pub fn get_mqtt_options(&self) -> MqttOptions {
+		let reconnection_options = ReconnectOptions::Always(10);
+		
+		let mut security = SecurityOptions::None;
+        if !self.brokeruser.is_empty() {
+        	security = SecurityOptions::UsernamePassword
+        			(self.brokeruser.clone(), self.brokerpassword.clone());
+        }
+		
+		MqttOptions::new("standard-sub", self.brokeraddress.clone(), self.brokerport)
+                                    .set_keep_alive(10)
+                                    .set_inflight(3)
+                                    .set_request_channel_capacity(3)
+                                    .set_reconnect_opts(reconnection_options)
+                                    .set_clean_session(false)
+                                    .set_security_opts(security)
+	}
+	
+	pub fn get_mqtt_options_with_id(&self, client_id: &str) -> MqttOptions {
+		let reconnection_options = ReconnectOptions::Always(10);
+		
+		let mut security = SecurityOptions::None;
+        if !self.brokeruser.is_empty() {
+        	security = SecurityOptions::UsernamePassword
+        			(self.brokeruser.clone(), self.brokerpassword.clone());
+        }
+		
+		MqttOptions::new(client_id, self.brokeraddress.clone(), self.brokerport)
+                                    .set_keep_alive(10)
+                                    .set_inflight(3)
+                                    .set_request_channel_capacity(3)
+                                    .set_reconnect_opts(reconnection_options)
+                                    .set_clean_session(false)
+                                    .set_security_opts(security)
+	}
+	
+	pub fn get_get_serialport_arc(&self) -> Arc<Mutex<Box<dyn serialport::SerialPort>>> {
+		self.port.clone()
+	}
+}
